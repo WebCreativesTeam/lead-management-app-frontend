08ce0db4399e270160eab6dd038a275cda4dbeed
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2024-02-09 23:19:49 +0530

    removed last week details

--- components/Dashboard/FollowUpCard/index.tsx
+++ components/Dashboard/FollowUpCard/index.tsx
@@ -4,7 +4,7 @@ import { IFollowUpCard } from './FollowUpCard.types';
 
 const FollowUpCard = ({ title, followUpPercentage, followups, lastWeekFollowUps, color = 'blue' }: IFollowUpCard) => {
     return (
-        <div className={`panel from-${color}-500 to-${color}-400 bg-gradient-to-r`} style={{ backgroundImage: `linear-gradient(to right, ${color}, ${color+"95"})` }}>
+        <div className={`panel from-${color}-500 to-${color}-400 bg-gradient-to-r`} style={{ backgroundImage: `linear-gradient(to right, ${color}, ${color + '95'})` }}>
             <div className="flex justify-between">
                 <div className="text-md font-semibold ltr:mr-1 rtl:ml-1">{title}</div>
             </div>
@@ -12,11 +12,11 @@ const FollowUpCard = ({ title, followUpPercentage, followups, lastWeekFollowUps,
                 <div className="text-3xl font-bold ltr:mr-3 rtl:ml-3"> {followups} </div>
                 {/* <div className="badge bg-white/30"> {followUpPercentage} </div> */}
             </div>
-            <div className="mt-5 flex items-center font-semibold">
+            {/* <div className="mt-5 flex items-center font-semibold">
                 <EyeIcon />
-                {/* Last Week */}
+                Last Week
                 {lastWeekFollowUps}
-            </div>
+            </div> */}
         </div>
     );
 };
